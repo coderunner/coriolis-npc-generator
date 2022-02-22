f42d6fdf955cff431b6be2d2a39bf9cd0d5fa7bd
Felix Trepanier <felixtrepanier@gmail.com>
2022-02-22 16:57:41 -0500

    Add template support

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { CoriolisNPCGenerator } from './generator.js';
 
 Hooks.once('ready', () => {
+  console.log('Coriolis NPC Generator | Ready');
   game.CoriolisNpcGenerator = new CoriolisNPCGenerator();
 });
