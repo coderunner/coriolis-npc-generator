8f6b5edf82c491cf5474623ea5cefe0955e54ea9
Felix Trepanier <felixtrepanier@gmail.com>
2022-02-22 16:45:32 -0500

    Add styles

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,7 @@ import { CoriolisNPCGenerator } from './generator.js';
 
 Hooks.once('ready', () => {
   game.CoriolisNpcGenerator = new CoriolisNPCGenerator();
+
+  //   console.log('Loading template...');
+  //   loadTemplates(['modules/coriolis-npc-generator/templates/npc.hbs']);
 });
